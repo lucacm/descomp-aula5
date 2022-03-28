b177469499dbc39f94ac49e3ab95ba0fd7a2792b
lucacm <lucacm@al.insper.edu.br>
2022-03-28 19:38:42 -0300

    atualizando pasta aula5

--- aula5/aula4.vhd
+++ aula5/aula4.vhd
@@ -1,7 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity aula5 is
+entity aula4 is
   -- Total de bits das entradas e saidas
   generic ( larguraDados : natural := 8;
         larguraEnderecos : natural := 9;
@@ -21,25 +21,20 @@ entity aula5 is
 end entity;
 
 
-architecture arquitetura of 5 is
+architecture arquitetura of aula4 is
 
 -- Faltam alguns sinais:
   signal chavesX_ULA_B       : std_logic_vector (larguraDados-1 downto 0);
   signal MUX_REG1            : std_logic_vector (larguraDados-1 downto 0);
-  signal MUX_JMP             : std_logic_vector (larguraDados-1 downto 0);
   signal REG1_ULA_A          : std_logic_vector (larguraDados-1 downto 0);
-  signal REG_END_RET         : std_logic_vector (larguraDados-1 downto 0);
   signal Saida_ULA           : std_logic_vector (larguraDados-1 downto 0);
   signal Sinais_Controle     : std_logic_vector (5 downto 0);
   signal Endereco            : std_logic_vector (8 downto 0);
   signal proxPC              : std_logic_vector (8 downto 0);
   signal Chave_Operacao_ULA  : std_logic;
   signal CLK                 : std_logic;
-  signal FlagJEQ             : std_logic;
-  signal SelMUX_A            : std_logic;
-  signal SelMUX_JMP          : std_logic_vector (1 downto 0);
+  signal SelMUX              : std_logic;
   signal Habilita_A          : std_logic;
-  signal Habilita_RET        : std_logic;
   signal Operacao_ULA        : std_logic_vector (1 downto 0);
   signal Ligacao_ROM_Decoder : std_logic_vector (3 downto 0);
   signal habLeituraMEM       : std_logic; 
@@ -65,33 +60,20 @@ detectorSub0: work.edgeDetector(bordaSubida)
         port map (clk => CLOCK_50, entrada => (not KEY(0)), saida => CLK);
 end generate;
 
--- O port map completo do MUX 2x1.
+-- O port map completo do MUX.
 MUX1 :  entity work.muxGenerico2x1  generic map (larguraDados => larguraDados)
         port map( entradaA_MUX => saidaRAM_entradaMUX,
                  entradaB_MUX =>  instrucao(7 downto 0),
-                 seletor_MUX => SelMUXA,
+                 seletor_MUX => SelMUX,
                  saida_MUX => MUX_REG1);
-					  
--- O port map completo do MUX 4x1.
-MUXJMP :  entity work.muxGenerico4x1  generic map (larguraDados => larguraDados)
-	  port map( 	entradaA_MUX =>  proxPC,
-                  entradaB_MUX =>  instrucao(7 downto 0),
-                  entradaC_MUX =>  REG_END_RET,
-                  entradaD_MUX =>  '0',
-                  seletor_MUX => SelMUXJMP,
-                  saida_MUX => MUX_JMP);
 
 -- O port map completo do Acumulador.
 REGA : entity work.registradorGenerico   generic map (larguraDados => larguraDados)
           port map (DIN => Saida_ULA, DOUT => REG1_ULA_A, ENABLE => Habilita_A, CLK => CLK, RST => '0');
-			 
- -- O port map completo do Acumulador.
-REG_RET : entity work.registradorGenerico   generic map (larguraDados => larguraDados)
-          port map (DIN => proxPC, DOUT => REG_END_RET, ENABLE => Habilita_RET, CLK => CLK, RST => '0');
 
 -- O port map completo do Program Counter.
 PC : entity work.registradorGenerico   generic map (larguraDados => tamanhoEnderecoROM)
-          port map (DIN => MUX_JMP, DOUT => Endereco, ENABLE => '1', CLK => CLK, RST => '0');
+          port map (DIN => proxPC, DOUT => Endereco, ENABLE => '1', CLK => CLK, RST => '0');
 
 incrementaPC :  entity work.somaConstante  generic map (larguraDados => tamanhoEnderecoROM, constante => 1)
         port map( entrada => Endereco, saida => proxPC);
@@ -99,7 +81,7 @@ incrementaPC :  entity work.somaConstante  generic map (larguraDados => tamanhoE
 
 -- O port map completo da ULA:
 ULA1 : entity work.ULASomaSub  generic map(larguraDados => larguraDados)
-          port map (entradaA => REG1_ULA_A, entradaB => MUX_REG1, saida => Saida_ULA, seletor => Operacao_ULA, flagZero => FlagJEQ );
+          port map (entradaA => REG1_ULA_A, entradaB => MUX_REG1, saida => Saida_ULA, seletor => Operacao_ULA);
 
 -- Falta acertar o conteudo da ROM (no arquivo memoriaROM.vhd)
 ROM1 : entity work.memoriaROM   generic map (dataWidth => tamanhoInstrucaoROM, addrWidth => tamanhoEnderecoROM)
@@ -111,10 +93,6 @@ DECODER : entity work.decoderInstru
 RAM1 : entity work.memoriaRAM   
           port map (addr => instrucao(7 downto 0), we => habEscritaMEM, re => habLeituraMEM, habilita => instrucao(8), dado_in => REG1_ULA_A, dado_out => saidaRAM_entradaMUX, clk => CLK);
 
-FLAG : entity work.registradorGenerico
-				port map(DIN => FlagJEQ, DOUT => FlagJEQ, ENABLE =>, CLK => CLK, RST => '0');
-			-- falta completar com o enable (é o que sai do decoder)	
-			 
 selMUX        <= Sinais_Controle(5);
 Habilita_A    <= Sinais_Controle(4);
 Operacao_ULA  <= Sinais_Controle(3 downto 2);
